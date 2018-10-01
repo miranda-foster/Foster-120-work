78833586e51934e22708d404d05c1ef1b22f642a
Miranda Foster <MFoster5559@scrippscollege.edu>
2018-09-30 19:02:07 -0600

    this is difficult~!

--- hw4/sketch.js
+++
@@ -1,9 +0,0 @@
-// Declare the setup function
-function setup() {
-
-    // create a canvas 600px wide and 400px high
-    createCanvas( 600, 400 );
-
-    // color the background 'pink'
-    background( 'pink' );
-}
